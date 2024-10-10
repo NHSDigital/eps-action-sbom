261bb44257e166b9e9d0e583df137d3475bb68c9
Jim Wild <james.wild6@nhs.net>
2024-10-10 10:32:46 +0000

    Refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:24.04
+FROM ubuntu:22.04
 
 RUN apt-get update \
     && export DEBIAN_FRONTEND=noninteractive \
@@ -30,43 +30,17 @@ RUN asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git; \
 # Install Grype
 RUN curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
 
-# For each supported npm version, asdf install, and install cyclonedx (latest versions)
+# Install Syft
+RUN curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
 
-ADD node18/.tool-versions /node_versions/node18/.tool-versions
-WORKDIR /node_versions/node18
-RUN asdf install
-RUN asdf exec npm install -g @cyclonedx/cyclonedx-npm
-RUN asdf exec python -m pip install cyclonedx-bom
-
-
-ADD node20/.tool-versions /node_versions/node20/.tool-versions
-WORKDIR /node_versions/node20
-RUN asdf install
-RUN asdf exec npm install -g @cyclonedx/cyclonedx-npm
-RUN asdf exec python -m pip install --no-cache-dir cyclonedx-bom
-
-
-ADD node22/.tool-versions /node_versions/node22/.tool-versions
-WORKDIR /node_versions/node22
-RUN asdf install
-RUN asdf exec npm install -g @cyclonedx/cyclonedx-npm
-RUN asdf exec python -m pip install --no-cache-dir cyclonedx-bom
-
-
-# Set the workdir to what we'll actually use
+# Set the working directory
 WORKDIR /working
 
-# And install cyclonedx in this asdf environment
-RUN echo "python 3.12.5" >> .tool-versions
-RUN asdf exec python -m pip install --no-cache-dir cyclonedx-bom
-RUN rm .tool-versions
-
-# Files to execute when the docker container starts up
+# Add entrypoint scripts
 ADD entrypoint.sh /entrypoint.sh
-ADD check-sbom-issues-against-ignores.sh /check-sbom-issues-against-ignores.sh 
+ADD check-sbom-issues-against-ignores.sh /check-sbom-issues-against-ignores.sh
 
-# Set the umask so that the files created by docker can be universally accessed. 
-# Lets the tests successfully teardown.
+# Set the umask
 RUN echo "umask 000" >> /etc/profile
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,71 +2,63 @@
 
 set -e
 
-
 # Remove any existing SBOMs
 rm -f ./sbom*.json
 
-# Get the .tool-versions for the correct node
-NODE_VERSION=${1:-'20'}
-cp /node_versions/node"${NODE_VERSION}"/.tool-versions .
-
-# If the current github workflow has installed asdf, it will pass the ASDF_DIR and ASDF_DATA_DIR
-# environment variables in to the action, overriding this docker container's installation and leaving it
-# unable to find the scripts.
-# Here, we check if these variables are set, and if necessary we copy in our local installation of asdf.
+# Fix ASDF environment variables if necessary
 if [ -n "${ASDF_DIR}" ]; then
-    echo "ASDF_DIR not set. Copying in local installation of asdf..."
+    echo "ASDF_DIR not set. Copying in local installation of ASDF..."
     mkdir -p "${ASDF_DIR}"
     cp -r /root/.asdf/* "${ASDF_DIR}"/
-    ls -lha "${ASDF_DIR}"
 fi
 asdf reshim
 
-# Scan the dependencies for NPM
-if [ -f "package.json" ] && [ -f "package-lock.json" ]; then
-    echo "Generating SBOM for Node.js..."
-    # The node_modules directory is needed for generating the SBOM
+# Install NPM dependencies if package.json exists
+if [ -f "package.json" ]; then
+    echo "Installing NPM dependencies..."
     asdf exec npm install
-    asdf exec cyclonedx-npm --output-format json --output-file sbom-node.json
-    echo "Done"
-else
-    echo "package.json and package-lock.json not found. Cannot generate Node.js SBOM."
 fi
 
-# Repeat the above steps for Python
-# Check if pyproject.toml (Poetry) or requirements.txt exists
+# Install Python dependencies
 if [ -f "pyproject.toml" ]; then
-    echo "Detected Poetry project. Generating SBOM using Poetry..."
-    asdf exec cyclonedx-py poetry > sbom-python-poetry.json
-fi 
-
-if [ -f "requirements.txt" ]; then
-    echo "Detected requirements.txt. Generating SBOM using pip..."
-    asdf exec cyclonedx-py requirements > sbom-python-pip.json
+    echo "Detected Poetry project. Installing dependencies..."
+    asdf exec poetry install
+elif [ -f "requirements.txt" ]; then
+    echo "Detected requirements.txt. Installing dependencies..."
+    asdf exec pip install -r requirements.txt
 fi
 
-echo "Done"
+# Generate SBOM for NPM packages
+if [ -f "package.json" ]; then
+    echo "Generating SBOM for NPM packages..."
+    syft dir:./ --catalogers npm-package-cataloger -o syft-json -o sbom-node.json
+    echo "Done"
+fi
 
-# For each sbom*.json file, scan it with Grype
+# Generate SBOM for Python packages
+if [ -f "pyproject.toml" ] || [ -f "requirements.txt" ]; then
+    echo "Generating SBOM for Python packages..."
+    syft dir:./ --catalogers python-package-cataloger -o syft-json -o sbom-python.json
+    echo "Done"
+fi
+
+# Scan each SBOM with Grype
 for sbom in ./sbom*.json; do
     if [ -s "$sbom" ]; then
-        echo "$sbom file exists and has data. Scanning with Grype..."
-        grype "sbom:$sbom" -o json > "$(basename "$sbom" .json)-analysis.json"
+        echo "$sbom exists and has data. Scanning with Grype..."
+        grype sbom:"$sbom" -o json > "$(basename "$sbom" .json)-analysis.json"
         echo "Done"
     fi
 done
 
-# Don't exit on failure until we check all files.
+# Allow script to continue even if errors occur
 set +e
-# Initialize an error flag
 error_occurred=false
 
-# Then compare to ignored issues
+# Compare analysis results with ignored issues
 for analysis in ./sbom*-analysis.json; do
     if [ -s "$analysis" ]; then
-        echo "$analysis file exists and has data. Comparing to ignored issues..."
-        
-        # Check the exit status of the last command
+        echo "$analysis exists and has data. Comparing to ignored issues..."
         if ! /check-sbom-issues-against-ignores.sh ./ignored_security_issues.json "$analysis"; then
             echo "Error: check-sbom-issues-against-ignores.sh failed for $analysis"
             error_occurred=true
